92c8e102bd3b0df5bd6dc6bee8bc90edd680fe32
hare1901 <hare1901@yandex.ru>
2023-02-16 21:26:47 +0300

    continue to suffer

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 import random
 
-class Ship():
-
-    def __init__(self):
-        x=y=-1
-        self.size = 10
-        self.map = [["." for _ in range(self.size)] for _ in range(self.size)]
-        self.map_cordin = [[[y,x]for x in range(self.size)] for y in range(self.size)]
-        self.ship1 = [[x,y],[x,y],[x,y],[x,y]]
-        self.ship2 = [[[x,y],[x,y]],[[x,y],[x,y]],[[x,y],[x,y]],[[x,y],[x,y],[x,y]],[[x,y],[x,y],[x,y],[x,y]]]
-
-    def one_ship_placement(self):
-        for i in range(len(self.ship1)):
-            while self.ship1[i][0] < 0:
-                x = random.randint(0, 9)
-                y = random.randint(0, 9)
-                if self.map[x][y] != "X":
-                    self.map[x][y] = 'X'
-                    self.ship1[i][0], self.ship1[i][1] = x+1,y+1
-                    print(self.ship1[i])
-
-    def ship_placement(self):
-
-
-    def map_p(self):
-        Gamer.map_print(self)
-
-
-
-
 class Gamer():
 
     def __init__(self):
         '''
         create playing field
         '''
+        x, y = -1, -1
+        self.ship1 = [[x, y], [x, y], [x, y], [x, y]]
+        self.ship2 = [[[x, y], [x, y]], [[x, y], [x, y]], [[x, y], [x, y]], [[x, y], [x, y], [x, y]],[[x, y], [x, y], [x, y], [x, y]]]
         self.size = 10
+        self.map = [["." for _ in range(self.size)] for _ in range(self.size)]
         self.radar = [["." for _ in range(self.size)] for _ in range(self.size)]
+
+    def chek_around_ship(self, x, y, *args):
+        '''
+        проверка на возможность занять точку палубой коробля
+        :return: true/false
+        '''
+        all_empty_coordinate = [[i,j] for i in [x-1, x, x+1] for j in [y-1, y, y+1] if self.map[i][j] != '0']
+        if len(all_empty_coordinate) == 9 and self.map[x][y] not in all_empty_coordinate:
+            print(all_empty_coordinate)
+            print(True)
+            return True
+        else:
+            print(False)
+            return False
+
+
+    def one_ship_placement(self):
+        for i in range(len(self.ship1)):
+            while self.ship1[i][0] < 0:
+                x = random.randint(1, 8)
+                y = random.randint(1, 8)
+                #print("Вв")
+                #x, y = int(input()), int(input())
+                if self.chek_around_ship(x, y):
+                    self.map[y][x] = '0'
+                    self.ship1[i][0], self.ship1[i][1] = x, y
+                    print(self.ship1[i])
+                else:
+                    print(False)
+                    continue
 
     def map_print(self):
         print("Map:")
@@ -46,7 +51,7 @@ class Gamer():
                     print("  ", end="")
                     continue
                 if x == -1 and y >= 0:
-                    print(str(y + 1).rjust(2), end=" ")
+                    print(str(y).rjust(2), end=" ")
                     continue
                 if x >= 0 and y == -1:
                     print(("A", "B", "C", "D", "E", "F", "G", "H", "I", "J")[x], end=' ')
@@ -70,6 +75,9 @@ class Gamer():
                 print(" " + str(self.radar[x][y]), end=' ')
             print("")
 
+
+
+
 class Game():
     '''
     Controll all actions
@@ -83,12 +91,12 @@ class Game():
 
 
 if __name__ == "__main__":
-    b = Ship()
+    b = Gamer()
     b.one_ship_placement()
+    b.map_print()
     # print(b.map_cordin)
     # print(b.ship1)
     # print(b.ship2)
-    print(b.map_p())
     # a = Gamer()
     # a.map_print()
     # a.radar_print()
